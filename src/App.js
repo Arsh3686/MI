965b068407ca9604772be2ef4ddb22429491a563
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-03-26 12:37:58 +0530

    Added New User's Search Field 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddToCart from "./addToCart/AddToCart";
 import "./App.css";
 import Contact from "./components/Contact";
 import ForgotPassword from "./components/ForgotPassword";
@@ -11,25 +10,10 @@ import Product from "./components/Product";
 import Productdetails from "./components/Product-details";
 import User from "./components/User";
 
-// import { Provider } from "react-redux";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import AddToCart from "./addToCart/AddToCart";
-// import CartHandler from "./addToCart/CartHandler";
-
 import store from "./store/Store";
-// import { Provider } from "react-redux";
-// import store from "./todoList/store";
-// import TodoList from "./todoList/TodoList";
-// import store from "./addToCart/cartStore";
-// import Price from "./addToCart/Price";
-// import Weather from "./Weather/Weather";
 
 const App = () => {
 	return (
-		// <Provider store={store}>
-		// 	<TodoList />
-		// </Provider>
-
 		<Provider store={store}>
 			<BrowserRouter>
 				<NavHandle />
@@ -48,17 +32,6 @@ const App = () => {
 				</Routes>
 			</BrowserRouter>
 		</Provider>
-
-		// <Provider store={store}>
-		// 	<BrowserRouter>
-		// 		<Routes>
-		// 			<Route path="/" element={<CartHandler />} />
-		// 			<Route path="/addToCart" element={<AddToCart />} />
-		// 			<Route path="/price" element={<Price />} />
-		// 			{/* <Route path="/" element={<Weather />} /> */}
-		// 		</Routes>
-		// 	</BrowserRouter>
-		// </Provider>
 	);
 };
 
